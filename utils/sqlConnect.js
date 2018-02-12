1d49754c6dc8e0f3a327e10f2dd39155748fa756
Bernal Gomez <d_bernalgomez@tss-hr420-dm20.fc.ca>
2018-02-12 13:42:14 -0500

    Conencting database

---
+++ utils/sqlConnect.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var config = require('../config');
+
+var connect = mysql.createConnection({
+
+  host : config.host,
+  port : config.port,
+  user : config.user,
+  password : config.password,
+  database : config.database
+
+});
+
+
+module.exports = connect;
