8fde2f0df337f2aad595609c337f72d812177cbc
Bernal Gomez <d_bernalgomez@tss-hr420-dm20.fc.ca>
2018-02-12 12:33:44 -0500

    adding index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  { name: "Jade", usefulness: "sucky" },
+  { name: "EJS", usefulness: "a little better" },
+  { name: "handlebars", usefulness: "awesome" }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: "I need vacations" });
+  res.render('index', {
+  title: 'Express',
+  message: "I need vacations",
+  templates: templateTypes
+  });
 });
 
 module.exports = router;
